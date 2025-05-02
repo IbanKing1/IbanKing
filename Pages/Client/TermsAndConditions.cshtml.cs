3c4fc11fb3ddaadae93eaa0699b2a003bddc5bbd
Andra <smarandache.andra.e3s@student.ucv.ro>
2025-05-02 19:32:55 +0300

    Every role has a TermsAndCondition page

---
+++ Pages/Client/TermsAndConditions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBanKing.Pages.Client
+{
+    public class TermsAndConditionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
