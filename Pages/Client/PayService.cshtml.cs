d241035d22ab82b73159207396dd4564cbe9fcca
Radu <164653790+RaduNDL@users.noreply.github.com>
2025-05-03 12:11:03 +0300

    Reapply "Css changes, tooltip and show balance"
    This reverts commit 52d228f988ec242435b4362ff2ae5c4d0773d2da.

--- Pages/Client/PayService.cshtml.cs
+++ Pages/Client/PayService.cshtml.cs
@@ -11,7 +11,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-
 namespace IBanKing.Pages.Client
 {
     public class PayServiceModel : PageModel
---
+++ wwwroot/js/homeclient.js
@@ -0,0 +1,52 @@
+﻿function copyIBAN(iban) {
+    navigator.clipboard.writeText(iban).then(function () {
+        alert("IBAN copied to clipboard: " + iban);
+    });
+}
+
+let balanceVisible = false;
+
+function toggleBalanceVisibility() {
+    const toggleButton = document.getElementById("toggleBalanceBtn");
+    if (toggleButton) toggleButton.blur();
+
+    balanceVisible = !balanceVisible;
+    const balances = document.querySelectorAll('.account-balance');
+    const eyeIcon = document.getElementById("eyeIcon");
+
+    balances.forEach(el => {
+        el.textContent = balanceVisible ? el.dataset.realvalue : '••••••';
+    });
+
+    if (balanceVisible) {
+        eyeIcon.outerHTML = `
+            <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg"
+                 width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8z"/>
+                <path d="M8 5a3 3 0 1 1 0 6a3 3 0 0 1 0-6z"/>
+            </svg>`;
+    } else {
+        eyeIcon.outerHTML = `
+            <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg"
+                 width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M13.359 11.238a9.469 9.469 0 0 0 2.273-3.238
+                         s-3-5.5-8-5.5a9.719 9.719 0 0 0-3.366.66L5.43 4.43
+                         a6.5 6.5 0 0 1 6.218 1.082 3 3 0 0 1-4.087 4.087
+                         6.5 6.5 0 0 1-1.082-6.218L2.293 1.707a1 1 0 0 0-1.414 1.414
+                         l12 12a1 1 0 0 0 1.414-1.414l-1.934-1.934z"/>
+            </svg>`;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const balances = document.querySelectorAll('.account-balance');
+    balances.forEach(el => {
+        el.dataset.realvalue = el.textContent.trim();
+        el.textContent = '••••••';
+    });
+
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
